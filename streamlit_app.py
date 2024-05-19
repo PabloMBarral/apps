d3bfbcd9cc1724b67ad9ab7dc61cccc4275547ce
Pablo Barral <pbarral@fi.uba.ar>
2024-05-18 21:25:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import CoolProp.CoolProp as cp
 
+# Notas
+# CoolProp usa pascales absolutos y kelvin, y devuelve en J / kg.
 
 # Título de la aplicación
 st.title("Calculador de entalpías")
 
-# Entradas para los dos números
-t = st.number_input("Ingrese la temperatura [°C]", value=0)
-p = st.number_input("Ingrese la presión [bar(a)]", value=0)
+# Entradas para la temperatura y presión como números decimales
+t = st.number_input("Ingrese la temperatura [°C]", value=0.0, step=0.01, format="%.2f")
+p = st.number_input("Ingrese la presión [bar(a)]", value=0.0, step=0.01, format="%.2f")
 
 
-# Botón para realizar la suma
-if st.button("Sumar"):
-    h = cp.PropsSI('H', 'P', p * 1e5, 'T', t + 273.15, 'Water')  / 1000 # Entalpía en [kJ/kg]
-    st.write(f"La entalpía a {t} °C y {p} bar(a) es {h} kJ/kg.")
+# Botón para calcular la entalpía
+if st.button("Calcular"):
+    h = cp.PropsSI('H', 'P', p * 1e5, 'T', t + 273.15, 'Water') / 1000  # Entalpía en [kJ/kg]
+    st.write(f"La entalpía a {t:.2f} °C y {p:.2f} bar(a) es {h:.2f} kJ/kg.")g.")
