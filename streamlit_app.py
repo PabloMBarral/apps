55c76e8cd78338fd86bb3216df0f16571815cf59
Pablo Barral <pbarral@fi.uba.ar>
2024-05-18 21:16:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
 import streamlit as st
-from fluids.units import *
-from thermo.units import Stream
+import CoolProp.CoolProp as cp
 
 
 # Título de la aplicación
 st.title("Calculador de entalpías")
 
 # Entradas para los dos números
-T = st.number_input("Ingrese la temperatura [°C]", value=0)
-P = st.number_input("Ingrese la presión [bar(a)]", value=0)
+t = st.number_input("Ingrese la temperatura [°C]", value=0)
+p = st.number_input("Ingrese la presión [bar(a)]", value=0)
 
-#T = T * u.degC
-#P = P * u.bar
 
 # Botón para realizar la suma
 if st.button("Sumar"):
-    steam = Stream('water', T = T, P = P, m = 40000) #* u.kg / u.hr)
-    st.write(f"La entalpía a {T} °C y {P} bar(a) es {steam.rho} kJ/kg.")
+    h = cp.PropsSI('H', 'P', p * 1e5, 'T', t + 273.15, 'Water')  / 1000 # Entalpía en [kJ/kg]
+    st.write(f"La entalpía a {t} °C y {p} bar(a) es {h} kJ/kg.")
