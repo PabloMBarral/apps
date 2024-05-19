a32529c23cd7bc758eda923d6cea85217ea26dfe
Pablo Barral <pbarral@fi.uba.ar>
2024-05-18 21:01:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from thermo.units import Stream
 
 
 # Título de la aplicación
-st.title("Suma de dos números")
+st.title("Calculador de entalpías")
 
 # Entradas para los dos números
 T = st.number_input("Ingrese la temperatura [°C]", value=0)
@@ -16,4 +16,4 @@ P = P * u.bar
 # Botón para realizar la suma
 if st.button("Sumar"):
     steam = Stream('water', T = T, P = P, m = 40000 * u.kg / u.hr)
-    st.write(f"La entalpía a {T} y {P} es {steam.h}")
+    st.write(f"La entalpía a {T} y {P} es {steam.rho}")
