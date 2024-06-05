97c64453ab74afcd062baad083794c598652471c
Pablo Barral <pbarral@fi.uba.ar>
2024-06-05 14:55:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,6 +110,8 @@ if tp_submit_button:
         st.write(f"Entalpía: {h:.2f} kJ/kg")
         st.write(f"Entropía: {s:.4f} kJ/(kg·K)")
         st.write(f"Título: {x:.2f}")
+    else
+        st.write(f"algo paso")
 """
 # Separador
 st.markdown("---")
