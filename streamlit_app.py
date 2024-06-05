5bf913839bb140c869e7baed6032ab5941e100eb
Pablo Barral <pbarral@fi.uba.ar>
2024-06-05 15:34:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,51 +83,65 @@ def calcular_propiedades(desde, **kwargs):
         st.error(f"Error en el cálculo: {e}")
         return None, None, None, None, None
 
-# Sección para diferentes combinaciones de propiedades
-#st.header("Calcular propiedades del agua")
-
-# Formulario para Temperatura y Presión
-st.write("### Temperatura y Presión")
-
-with st.form(key='tp_form'):
-    t = st.number_input("Ingrese la temperatura [°C]", value=0.0, step=0.01, format="%.2f")
-    p_t = st.number_input("Ingrese la presión [bar(a)]", value=1.0, step=0.01, format="%.2f")
-    tp_submit_button = st.form_submit_button(label='Calcular desde Temperatura y Presión')
-
-if tp_submit_button:
-    t, p, h, s, x = calcular_propiedades('TP', t=t, p=p_t)
-
-    if t is not None:
-        st.write(f"Resultados a {t:.2f} °C y {p:.2f} bar(a):")
-        st.write(f"Entalpía: {h:.2f} kJ/kg")
-        st.write(f"Entropía: {s:.4f} kJ/(kg·K)")
-        st.write(f"Título: {x:.2f}")
-    else:
-        st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 
-# Separador
-st.markdown("---")
+# Formulario para seleccionar la opción
+option = st.radio("Seleccione una opción:", ("t y p", 
+                                             "p y h",
+                                             "h y s",
+                                             "p y x",
+                                             "t y x",
+                                             "p y s",
+                                             "t y s"
+                                             ))
 
-# Formulario para Presión y Entalpía
-st.write("## Presión y Entalpía")
-with st.form(key='ph_form'):
-    h = st.number_input("Ingrese la entalpía [kJ/kg]", value=0.0, step=0.01, format="%.2f")
-    p_h = st.number_input("Ingrese la presión [bar(a)]", value=1.0, step=0.01, format="%.2f")
-    ph_submit_button = st.form_submit_button(label='Calcular desde Presión y Entalpía')
+if option == 't y p':
 
-if ph_submit_button:
-    t, p, h, s, x = calcular_propiedades('PH', h=h, p=p_h)
-    if t is not None:
-        st.write(f"Resultados a {h:.2f} kJ/kg y {p:.2f} bar(a):")
-        st.write(f"Temperatura: {t:.2f} °C")
-        st.write(f"Entropía: {s:.4f} kJ/(kg·K)")
-        st.write(f"Título: {x:.2f}")
-    else:
-        st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
+    # Formulario para Temperatura y Presión
+    st.write("### Temperatura y Presión")
+
+    with st.form(key='tp_form'):
+        t = st.number_input("Ingrese la temperatura [°C]", value=0.0, step=0.01, format="%.2f")
+        p_t = st.number_input("Ingrese la presión [bar(a)]", value=1.0, step=0.01, format="%.2f")
+        tp_submit_button = st.form_submit_button(label='Calcular desde Temperatura y Presión')
+
+    if tp_submit_button:
+        t, p, h, s, x = calcular_propiedades('TP', t=t, p=p_t)
+
+        if t is not None:
+            st.write(f"Resultados a {t:.2f} °C y {p:.2f} bar(a):")
+            st.write(f"Entalpía: {h:.2f} kJ/kg")
+            st.write(f"Entropía: {s:.4f} kJ/(kg·K)")
+            st.write(f"Título: {x:.2f}")
+        else:
+            st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
+
 
 # Separador
-st.markdown("---")
+# st.markdown("---")
+
+elif option == 'p y h':
+
+
+    # Formulario para Presión y Entalpía
+    st.write("## Presión y Entalpía")
+    with st.form(key='ph_form'):
+        h = st.number_input("Ingrese la entalpía [kJ/kg]", value=0.0, step=0.01, format="%.2f")
+        p_h = st.number_input("Ingrese la presión [bar(a)]", value=1.0, step=0.01, format="%.2f")
+        ph_submit_button = st.form_submit_button(label='Calcular desde Presión y Entalpía')
+
+    if ph_submit_button:
+        t, p, h, s, x = calcular_propiedades('PH', h=h, p=p_h)
+        if t is not None:
+            st.write(f"Resultados a {h:.2f} kJ/kg y {p:.2f} bar(a):")
+            st.write(f"Temperatura: {t:.2f} °C")
+            st.write(f"Entropía: {s:.4f} kJ/(kg·K)")
+            st.write(f"Título: {x:.2f}")
+        else:
+            st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
+
+# Separador
+#st.markdown("---")
 
 # Formulario para Entalpía y Entropía
 st.write("### Entalpía y Entropía")
@@ -147,7 +161,7 @@ if hs_submit_button:
         st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 # Separador
-st.markdown("---")
+#st.markdown("---")
 
 # Formulario para Presión y Título
 st.write("### Presión y Título")
@@ -167,7 +181,7 @@ if px_submit_button:
         st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 # Separador
-st.markdown("---")
+#st.markdown("---")
 
 # Formulario para Temperatura y Título
 st.write("### Temperatura y Título")
@@ -187,7 +201,7 @@ if tx_submit_button:
         st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 # Separador
-st.markdown("---")
+#st.markdown("---")
 
 # Formulario para Presión y Entropía
 st.write("### Presión y Entropía")
@@ -207,7 +221,7 @@ if ps_submit_button:
         st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 # Separador
-st.markdown("---")
+#st.markdown("---")
 
 # Formulario para Temperatura y Entropía
 st.write("### Temperatura y Entropía")
@@ -227,7 +241,7 @@ if ts_submit_button:
         st.write(f"Revisá que sean coherentes los valores ingresados, y volvé a intentarlo.")
 
 # Separador
-st.markdown("---")
+#st.markdown("---")
 
 # Texto adicional
 st.write("Desarrollado por Pablo M. Barral. Versión: 0.01. Contacto: pbarral@fi.uba.ar. Powered by CoolProp.")
